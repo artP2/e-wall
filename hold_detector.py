ee9c07edb3e3388d771f8390faada545c7fca452
Arthur Pin <artP2@tuta.io>
2025-07-01 10:19:46 -0300

    organize code

--- hold_detector.py
+++ hold_detector.py
@@ -13,11 +13,13 @@ class Imshower:
         self.axis = axis
         self.i = 0
 
-    def __call__(self, img) -> None:
+    def __call__(self, img, title=None) -> None:
         if (img.dtype is bool):
             self.axis[self.i].imshow(img)
         else:
             self.axis[self.i].imshow(img, cmap='gray')
+        if title:
+            self.axis[self.i].set_title(title)
         self.i += 1
 
 
@@ -39,31 +41,30 @@ def main(img_path):
     axs= axs.flatten()
 
     imshow = Imshower(axs)
-    imshow(img)
+    imshow(img, "Original")
 
+    # preprocessing
     img_blur = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=0)
-    #imshow(img)
-
     img_lab = cv2.cvtColor(img_blur, cv2.COLOR_RGB2LAB)
-    #imshow(img_lab)
 
+    # superpixel segmentation using SLIC
     slic = cv2.ximgproc.createSuperpixelSLIC(img_lab, algorithm=cv2.ximgproc.SLIC)
     slic.iterate(10)
     slic.enforceLabelConnectivity(min_element_size=25)
 
+    # superpixel boundaries
     slic_mask = slic.getLabelContourMask(thick_line=False)
     img_slic = img.copy()
     img_slic[slic_mask == 255] = [0,0,0]
-    imshow(img_slic)
+    imshow(img_slic, "Superpixels")
 
     labels = slic.getLabels()
 
-    #img_median = img.copy()
     img_median = cv2.cvtColor(img, cv2.COLOR_RGB2LAB).astype(np.float32)
     img_median[:, :, 0] *= 0.1
     img_median[:, :, 1] *= 1.0
     img_median[:, :, 2] *= 1.0
-    #img_median = img_median.astype(np.uint8)
+
     for i in range(slic.getNumberOfSuperpixels()):
         mask = labels == i
         img_median[mask] = img_median[mask].mean(axis=0)
@@ -80,27 +81,21 @@ def main(img_path):
     label = label.flatten()
     res = center[label]
     res2 = res.reshape((img.shape))
-    imshow(cv2.cvtColor(res2, cv2.COLOR_LAB2RGB))
+    imshow(cv2.cvtColor(res2, cv2.COLOR_LAB2RGB), "K-means")
 
     uniq, counts = np.unique(label, return_counts=True)
     sort_indices = np.argsort(-counts)
     bots = uniq[sort_indices[K//2:]]
+    
     mask = np.zeros(center.shape[0])
     mask[bots] = 255
     mask = mask[label].reshape(img.shape[:2])
     mask = mask.astype(np.uint8)
     mask = cv2.GaussianBlur(mask, ksize=(3,3), sigmaX=0)
-    imshow(mask)
+    imshow(mask, "Binary Mask")
 
     holds = cv2.bitwise_and(img, img, mask=mask)
-    imshow(holds)
-
-
-    #lsc = cv2.ximgproc.SuperpixelLSC()
-    #lsc.iterate()
-    #lsc = cv2.bitwise_not(lsc.getLabelContourMask())
-    #img_lsc = cv2.bitwise_and(img, img, mask=slic_mask)
-    #imshow(img_lsc)
+    imshow(holds, "Final Result")
 
     plt.tight_layout()
     plt.show()
